41b7ea0644726800b7b8866058400af70f288742
Jaysig <jasontsigmon@gmail.com>
2016-01-23 18:50:05 -0500

    scope

--- functions.js
+++ functions.js
@@ -1,1 +1,15 @@
 //side effect is anything that's not returned from a function
+
+//arguments will return all of the arguments in a function 
+//Array.prototype.slice.call(arguments, 0);  - converts arguments into an array 
+
+//Constructor
+function AnimalMaker(name){
+	return {
+		speak: function(){
+			console.log('my name is ' + name);
+		}
+	};
+}
+
+//Block Scope - Does not exist in JS
